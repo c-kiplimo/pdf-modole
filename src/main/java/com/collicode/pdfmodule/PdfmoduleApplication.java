8e09218daa71c3217afdbb093533ae9036fa7f78
c-kiplimo <kiplimocollins855@gmail.com>
2023-12-11 18:45:02 +0200

    initial commit '

---
+++ src/main/java/com/collicode/pdfmodule/PdfmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.collicode.pdfmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collicode/pdfmodule/PdfmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collicode.pdfmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
